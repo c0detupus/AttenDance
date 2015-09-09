df909558f66b2470dc2b2ba511fbb9bdd3370822
kajri <qu.kairui@gmail.com>
2015-09-09 16:01:17 +0200

    Changed dropdown to dynamically with LinkedHashMap

--- AttenDanceList-web/src/main/java/afk/StatsBean.java
+++ AttenDanceList-web/src/main/java/afk/StatsBean.java
@@ -5,21 +5,126 @@
  */
 package afk;
 
-import java.util.List;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
-import javax.faces.model.SelectItem;
+import javax.faces.event.ValueChangeEvent;
 
 /**
  *
  * @author c0detupus
  */
 @ManagedBean(name = "statsBean")
-@SessionScoped
+@RequestScoped
 public class StatsBean {
-    
-    private List coursesList;
+
+//    private Courses courses;
+//    private List<Courses> coursesList;
     private String currentCourse;
+    private int currentMonth;
+    private String currentDay;
+    private int currentYear;
+
+    public int getCurrentYear() {
+        return currentYear;
+    }
+
+    public void setCurrentYear(int currentYear) {
+        this.currentYear = currentYear;
+    }
+
+    private Map<String, Integer> yearMap;
+    private Map<String, String> coursesMap;
+    private Map<String, Integer> monthMap;
+    private Map<String, String> dayMap;
+
+    public String getCurrentDay() {
+        return currentDay;
+    }
+
+    public void setCurrentDay(String currentDay) {
+        this.currentDay = currentDay;
+    }
+
+    public int getCurrentMonth() {
+        return currentMonth;
+    }
+
+    public void setCurrentMonth(int currentMonth) {
+        this.currentMonth = currentMonth;
+    }
+
+    @PostConstruct
+    public void init() {
+        //will populate the map depending on database
+        //hardcoded for now
+
+        coursesMap = new LinkedHashMap<>();
+        coursesMap.put("Java EE", "Java EE"); //label, value
+        coursesMap.put("JavaScript", "JavaScript");
+        coursesMap.put("CSS", "CSS");
+
+        monthMap = new LinkedHashMap<>();
+        monthMap.put("January", 0);
+        monthMap.put("February", 1);
+        monthMap.put("Mars", 2);
+
+        yearMap = new LinkedHashMap<>();
+        yearMap.put("2014", 2014);
+        yearMap.put("2015", 2015);
+        yearMap.put("2016", 2016);
+        populateDayMap();
+//        coursesList = new ArrayList<>();
+//        this.coursesList.add(new Courses("Java EE", 100));
+//        this.coursesList.add(new Courses("JavaScript", 50));
+//        this.coursesList.add(new Courses("CSS", 50));
+    }
+
+    public void submit() {
+        populateDayMap();
+    }
+
+    public void onMonthSelect(ValueChangeEvent e) {
+//        String temp = e.getNewValue().toString();
+//        currentMonth = Integer.parseInt(temp);
+        populateDayMap();
+    }
+
+    //will populate dayMap depending on which month is selected.
+    //
+    private void populateDayMap() {
+        int iDay = 1;
+
+        Calendar cal = new GregorianCalendar(currentYear, currentMonth, iDay);
+        int daysInMonth = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
+
+        dayMap = new LinkedHashMap<>();
+        for (int i = 1; i <= daysInMonth; i++) {
+            String temp = Integer.toString(i);
+            dayMap.put(temp, temp);
+        }
+    }
+
+    public Map<String, Integer> getYearMap() {
+        return yearMap;
+    }
+
+    public Map<String, String> getDayMap() {
+        return dayMap;
+    }
+
+    public Map<String, Integer> getMonthMap() {
+        return monthMap;
+    }
+
+    public Map<String, String> getCoursesMap() {
+        return coursesMap;
+    }
 
     public String getCurrentCourse() {
         return currentCourse;
@@ -28,18 +133,4 @@ public class StatsBean {
     public void setCurrentCourse(String currentCourse) {
         this.currentCourse = currentCourse;
     }
-
-    public List getCoursesList() {
-        //will populate the list depending on database
-        //hardcoded for now
-        this.coursesList.add(new SelectItem("Java EE","Java EE"));
-        this.coursesList.add(new SelectItem("JavaScript","JavaScript"));
-        this.coursesList.add(new SelectItem("CSS","CSS"));
-        return coursesList;
-    }
-
-//    public void setCoursesList(List coursesList) {
-//        this.coursesList = coursesList;
-//    }
-//    
 }
---
+++ AttenDanceList-web/src/main/java/model/Courses.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author kajri.qu
+ */
+public class Courses {
+
+    private String name;
+    private int points;
+
+    public Courses(String name, int points) {
+        name = this.name;
+        points = this.points;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+}
