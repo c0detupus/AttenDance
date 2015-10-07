d38b64ce899d0fe20c91b11003971f57f9a91489
Kajri Qu <qu.kairui@gmail.com>
2015-10-07 14:01:40 +0200

    added checkbox list for attendance

--- AttenDanceList-web/src/main/java/afk/AttendanceListBean.java
+++ AttenDanceList-web/src/main/java/afk/AttendanceListBean.java
@@ -6,8 +6,10 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 import model.Course;
 import model.Student;
 
@@ -16,6 +18,7 @@ import model.Student;
  * @author valance
  */
 @ManagedBean(name = "attendanceListBean")
+@ViewScoped
 public class AttendanceListBean implements Serializable {
 
     @EJB
@@ -23,6 +26,7 @@ public class AttendanceListBean implements Serializable {
 
     private List<Student> students;
     private List<Student> selectedStudent;
+
     private List<Course> courseList;
 
     private String selectedCourse;
@@ -30,19 +34,24 @@ public class AttendanceListBean implements Serializable {
     //String in Map is used to check First name, should be replaced to Long/Integer to check id/ssn
     private final Map<String, Boolean> selectedSSN = new HashMap<>();
 
-    //hardcoded, replace with db queries or helper class
+
+    @PostConstruct
+    public void init() {
+//        selectedStudent = new ArrayList<>();
+
+    }
+
     public List<Student> getStudents() {
+        if (selectedCourse != null) {
+            renderList();
+        }
         return students;
     }
 
     public void renderList() {
-        try {
-            Course course;
-            course = Helper.courseTOConverter(services.getCourseService().getCourse(Long.valueOf(selectedCourse), true), true);
-            students = course.getStudents();
-        } catch (NullPointerException e) {
-
-        }
+        Course course;
+        course = Helper.courseTOConverter(services.getCourseService().getCourse(Long.valueOf(selectedCourse), true), true);
+        students = course.getStudents();
     }
 
     public List<Course> getCourseList() {
@@ -50,13 +59,19 @@ public class AttendanceListBean implements Serializable {
         courseList = Helper.courseTOListConverter(services.getCourseService().getAll(), false);
         return courseList;
     }
-
+    
+    //ignore for now
     public void getSelectedItems() {
         // Get selected items.
+        renderList();
         selectedStudent = new ArrayList<>();
+        System.out.println("Att selectedItems: " + students);
         for (Student s : students) {
             //replace firstname to id/ssn
+            System.out.println("selectedItems map: " + selectedSSN.toString());
             if (selectedSSN.get(s.getFirstName())) {
+                System.out.println("selectedItems id: " + s.getId());
+                System.out.println("selectedItems name: " + s.getFirstName());
                 selectedStudent.add(s);
             }
         }
@@ -64,11 +79,24 @@ public class AttendanceListBean implements Serializable {
     }
 
     public void submit() {
-        getSelectedItems();
-        for (Student s : selectedStudent) {
-            System.out.println("selectedStudent: " + s.getFirstName());
-        }
+
+        System.out.println("Submit called()");
+//        getSelectedItems();
+//        for (Student s : selectedStudent) {
+//            System.out.println("selectedStudent: " + s.getFirstName());
+//        }
         //do things with selectedStudent???
+        System.out.println("selected Student: " + selectedStudent);
+        System.out.println("selected Student size: " + selectedStudent.size());
+//        System.out.println("selected datatable: " + datatable);
+    }
+
+    public List<Student> getSelectedStudent() {
+        return selectedStudent;
+    }
+
+    public void setSelectedStudent(List<Student> selectedStudent) {
+        this.selectedStudent = selectedStudent;
     }
 
     public Map<String, Boolean> getSelectedSSN() {
